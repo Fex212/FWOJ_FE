9fee66d871432c0a3fc592157a33da722fdca4f1
teleport-10032 <58069438+teleport-10032@users.noreply.github.com>
2020-10-19 21:58:31 +0800

    update

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -38,7 +38,7 @@
             <div slot="footer" class="dialog-footer">
                 <el-button @click="resetLoginForm" size="small">重 置</el-button>
                 <el-button @click="resetLoginFormAndClose" size="small">取 消</el-button>
-                <el-button type="primary" @click="resetLoginFormAndClose" size="small">确 定</el-button>
+                <el-button type="primary" @click="login" size="small">确 定</el-button>
             </div>
         </el-dialog>
         <!--     登陆 -->
@@ -177,7 +177,6 @@
             }
         },
         created() {
-            console.log(window.sessionStorage.getItem('activePath'));
             if (window.sessionStorage.getItem('activePath') == null)
                 window.sessionStorage.setItem('activePath', '/announcementList');
             this.activePath = window.sessionStorage.getItem('activePath')
@@ -202,6 +201,24 @@
             saveNavState(activePath) {
                 window.sessionStorage.setItem('activePath', activePath);
                 this.activePath = activePath;
+            },
+            login() {
+                //预验证
+                this.$refs.loginFormRef.validate(async valid => {
+                    //未通过则return
+                    if (!valid) return
+                    //通过
+                    const { data: res } = await this.$http.post('login', this.loginForm)
+                    if (res.meta.status !== 200) return this.$message.error('登录失败！')
+                    this.$message.success('登录成功')
+                    // 1. 将登录成功之后的 token，保存到客户端的 sessionStorage 中
+                    //   1.1 项目中出了登录之外的其他API接口，必须在登录之后才能访问
+                    //   1.2 token 只应在当前网站打开期间生效，所以将 token 保存在 sessionStorage 中
+                    console.log(res)
+                    window.sessionStorage.setItem('token', res.data.token)
+                    // 2. 通过编程式导航跳转到后台主页，路由地址是 /home
+                    this.$router.push('/home')
+                })
             }
         }
     }
